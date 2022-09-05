0dd4125367fdc60a73decc7f37151bc6bf995018
Kendy Song <pl70jwz@eduvaud.ch>
2022-09-05 11:18:30 +0200

    implementing application interface

--- include/Applicaion.hpp
+++ include/Applicaion.hpp
@@ -1,2 +1,17 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
+
+#pragma once
+class Application
+{
+private :
+    GLFWwindow* _window;
+    static Application* _application;
+
+    Application();
+
+public :
+    static Application* GetInstance() const noexcept;
+    
+    int Run() const noexcept;
+};
