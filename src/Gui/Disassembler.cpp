e76e6773d6861820c4c197830376ed7e7a8c17fc
Kendy <kndysong@gmail.com>
2022-09-29 11:32:10 +0200

    decode and display all instructions

--- src/Gui/Disassembler.cpp
+++ src/Gui/Disassembler.cpp
@@ -107,6 +107,22 @@ void Disassembler::DisassembleInstruction(std::uint16_t opCode)
         ImGui::Text("JP 0x%X", opCode & 0x0FFF);
         break;
 
+    case 0x2000 :
+        ImGui::Text("CALL 0x%X", opCode & 0x0FFF);
+        break;
+
+    case 0x3000 :
+        ImGui::Text("SE V[%X], %X", (opCode & 0x0F00) >> 8, opCode & 0x00FF);
+        break;
+
+    case 0x4000 :
+        ImGui::Text("SNE V[%X], %X", (opCode & 0x0F00) >> 8, opCode & 0x00FF);
+        break;
+
+    case 0x5000 :
+        ImGui::Text("SE V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+        break;
+
     case 0x6000 :
         ImGui::Text("LD V[%X], %X", (opCode & 0x0F00) >> 8, opCode & 0x00FF);
         break;
@@ -115,13 +131,122 @@ void Disassembler::DisassembleInstruction(std::uint16_t opCode)
         ImGui::Text("ADD V[%X], %X", (opCode & 0x0F00) >> 8, opCode & 0x00FF);
         break;
 
+    case 0x8000 :
+        switch (opCode & 0x000F)
+        {
+            case 0x0000 :
+                ImGui::Text("LD V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+                break;
+
+            case 0x0001 :
+                ImGui::Text("OR V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+                break;
+
+            case 0x0002 :
+                ImGui::Text("AND V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+                break;
+
+            case 0x0003 :
+                ImGui::Text("XOR V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+                break;
+
+            case 0x0004 :   
+                ImGui::Text("ADD V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+                break;
+
+            case 0x0005 :
+                ImGui::Text("SUB V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+                break;
+
+            case 0x0006 :
+                ImGui::Text("SHR V[%X]", (opCode & 0x0F00) >> 8);
+                break;
+
+            case 0x0007 :
+                ImGui::Text("SUBN V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+                break;
+
+            case 0x000E :
+                ImGui::Text("SHL V[%X]", (opCode & 0x0F00) >> 8);
+                break;
+        }
+        break;
+
+    case 0x9000 :
+        ImGui::Text("SNE V[%X], V[%X]", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4);
+        break;
+
     case 0xA000 :
         ImGui::Text("LD I, %X", opCode & 0x0FFF);
+        break;
+
+    case 0xB000 :
+        ImGui::Text("JP V[0], %X", opCode & 0x0FFF);
+        break;
+
+    case 0xC000 :
+        ImGui::Text("RND V[%X], %X", (opCode & 0x0F00) >> 8, opCode & 0x0FF);
         break;
 
     case 0xD000 :
         ImGui::Text("DRW V[%X], V[%X], %X", (opCode & 0x0F00) >> 8, (opCode & 0x00F0) >> 4, opCode & 0x000F);
         break;
+
+    case 0xE000 :
+        switch (opCode & 0x000F)
+        {
+        case 0x000E:
+            ImGui::Text("SKP V[%X]", (opCode & 0x0F00) >> 8);
+            break;
+
+        case 0x0001:
+            ImGui::Text("SKNP V[%X]", (opCode & 0x0F00) >> 8);
+            break;
+        }
+        break;
+        
+    case 0xF000 :
+        switch (opCode & 0x00FF)
+        {
+            case 0x007 :
+                ImGui::Text("LD V[%X], DT", (opCode & 0x0F00) >> 8);
+                break;
+            
+            case 0x000A :
+                ImGui::Text("LD V[%X], K", (opCode & 0x0F00) >> 8);
+                break;
+
+            case 0x0015 :
+                ImGui::Text("LD DT, V[%X]", (opCode & 0x0F00) >> 8);
+                break;
+
+            case 0x0018 :
+                ImGui::Text("LD ST, V[%X]", (opCode & 0x0F00) >> 8);
+                break;
+
+            case 0x001E :
+                ImGui::Text("ADD I, V[%X]", (opCode & 0x0F00) >> 8);
+                break;
+
+            case 0x0029 :
+                ImGui::Text("ADD F, V[%X]", (opCode & 0x0F00) >> 8);
+                break;
+
+            case 0x0033 :
+                ImGui::Text("ADD B, V[%X]", (opCode & 0x0F00) >> 8);    
+                break;
+
+            case 0x055 :
+                ImGui::Text("LD [%X], V[%X]", CPU::GetInstance()->GetIndex(), (opCode & 0x0F00) >> 8);
+                break;
+
+            case 0x0065 :
+                ImGui::Text("LD V[%X], [%X]",  (opCode & 0x0F00) >> 8, CPU::GetInstance()->GetIndex());
+                break;
+        }
+        break;
     }
+
+    
 
 }
